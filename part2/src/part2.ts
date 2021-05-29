6f1c64b09956df76a06bfeddabb4ffce892f0dd8
Ido <idorev@post.bgu.ac.il>
2021-05-29 14:08:30 +0300

    2.4 done

--- part2/src/part2.ts
+++ part2/src/part2.ts
@@ -8,33 +8,35 @@ type PromisedStore<K, V> = {
   delete(key: K): Promise<void>;
 };
 
-type StoreBody<K, V> = Map<K,V>; // Promise Store DB type
+type StoreBody<K, V> = Map<K, V>; // Promise Store DB type
 
 export function makePromisedStore<K, V>(): PromisedStore<K, V> {
-  const promiseStoreDB: StoreBody<K, V> = new Map<K,V>();
+  const promiseStoreDB: StoreBody<K, V> = new Map<K, V>();
   return {
     get(key: K) {
       return new Promise<V>((resolve, reject) => {
         const returendVal = promiseStoreDB.get(key);
-        returendVal!==undefined? resolve(returendVal) : reject(MISSING_KEY);
+        returendVal !== undefined ? resolve(returendVal) : reject(MISSING_KEY);
       });
     },
     set(key: K, value: V) {
       return new Promise<void>((resolve) => {
-        promiseStoreDB.set(key,value);
+        promiseStoreDB.set(key, value);
         resolve();
       });
     },
     delete(key: K) {
       return new Promise<void>((resolve, reject) => {
-        promiseStoreDB.delete(key)?
-        resolve() : reject(MISSING_KEY);
+        promiseStoreDB.delete(key) ? resolve() : reject(MISSING_KEY);
       });
     },
   };
 }
 
-export function getAll<K, V>(store: PromisedStore<K, V>, keys: K[]): Promise<V[]> {
+export function getAll<K, V>(
+  store: PromisedStore<K, V>,
+  keys: K[]
+): Promise<V[]> {
   return Promise.all(keys.map(store.get)); // gets an array of promises and apling Promis.all
 }
 
@@ -42,100 +44,82 @@ export function getAll<K, V>(store: PromisedStore<K, V>, keys: K[]): Promise<V[]
 export function asycMemo<T, R>(f: (param: T) => R): (param: T) => Promise<R> {
   const cache = makePromisedStore<T, R>();
   return async (param) => {
-      try {
-         const cached_value: R = await cache.get(param);
-          return cached_value 
-      } catch (error) {
-        const value = f(param)
-        cache.set(param, value)
-        return value
-      }
-  }
+    try {
+      const cached_value: R = await cache.get(param);
+      return cached_value;
+    } catch (error) {
+      const value = f(param);
+      cache.set(param, value);
+      return value;
+    }
+  };
 }
 // ! ! ! ! :( in the pdf
 
 // why the wrraped function return Promise<R>
-// Async functions always return a promise. 
+// Async functions always return a promise.
 // If the return value of an async function is not explicitly a promise, it will be implicitly wrapped in a promise.
 // in our case we want the value of the Chashe memory with the Type R
 // to understand why async always return Promise : ??? is to keep asynchrony all the following applications
 
-
 /* 2.3 */
-export function lazyFilter<T>(genFn: () => Generator<T>, filterFn:(v:T) => boolean): ()=> Generator<T> {
-  const filterGen=  function* (genFn: ()=> Generator<T>, filter: (v:T) => boolean) {
-      const gen = genFn()
-      for (let x of gen) {
-          if (filter(x)) {
-              yield x;
-          }
+export function lazyFilter<T>(
+  genFn: () => Generator<T>,
+  filterFn: (v: T) => boolean
+): () => Generator<T> {
+  const filterGen = function* (
+    genFn: () => Generator<T>,
+    filter: (v: T) => boolean
+  ) {
+    const gen = genFn();
+    for (let x of gen) {
+      if (filter(x)) {
+        yield x;
       }
     }
-    return ()=> filterGen(genFn,filterFn);
+  };
+  return () => filterGen(genFn, filterFn);
 }
-export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: (v:T) => R):()=> Generator<R> {
 
-const MapGen= function* (genFn: ()=> Generator<T>, map: (v:T)=> R){
-   const gen = genFn()
-   for (let x of gen) {
-     yield map(x);
-   }
+export function lazyMap<T, R>(
+  genFn: () => Generator<T>,
+  mapFn: (v: T) => R
+): () => Generator<R> {
+  const MapGen = function* (genFn: () => Generator<T>, map: (v: T) => R) {
+    const gen = genFn();
+    for (let x of gen) {
+      yield map(x);
+    }
+  };
+  return () => MapGen(genFn, mapFn);
 }
-return ()=> MapGen(genFn,mapFn);
- }
 
 /* 2.4 */
 // you can use 'any' in this question
-const wait2sec = async (p:Promise<any>, counter:number): Promise<any> => {
-  if (counter===3) throw Error
-  try {
-  return new Promise((resolve)=> {
-  setTimeout(async () => {
-  const x = await p
-  resolve(x)}
-  ,2000)
-    })
+
+const wait2Sec = async (): Promise<void> =>
+  new Promise((res: VoidFunction) => setTimeout(res, 2000));
+
+export async function asyncWaterfallWithRetry(
+  fns: [() => Promise<any>, ...((param: any) => Promise<any>)[]]
+): Promise<any> {
+  let returendVal = undefined;
+  for (let currAplliedFunc of fns) {
+    try {
+      returendVal = await currAplliedFunc(returendVal);
+    } catch {
+      await wait2Sec();
+      try {
+        returendVal = await currAplliedFunc(returendVal);
+      } catch {
+        await wait2Sec();
+        try {
+          returendVal = await currAplliedFunc(returendVal);
+        } catch (err) {
+          returendVal = err;
+        }
+      }
+    }
   }
-  catch{
-     wait2sec(p,counter+1)
-  }
+  return returendVal;
 }
- 
-// const wait2Sec = async <T>(prom: Promise<T>):Promise<T> =>{
-//  return new Promise<T>((resolve, reject)=>{})
-//  );
-//  const val = async ()=>{try{return await prom}catch(err){return err}}
-//  return val(); 
-//  setTimeout(_=>,2000);
-//   try{
-//     return setTimeout( async ()=>{
-//       const retVal:T = await prom;
-//       return new Promise<T>(retVal);
-//   },2000);}
-//   catch(err){
-//     try{
-//         return setTimeout( async ()=>{
-//       const retVal:T = await prom;
-//       return new Promise<T>(retVal);
-//   },2000);}
-//   catch(err){
-//     return new Promise(err);
-//   }
-//  }
-// }
-
-
- export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...((param:any)=> Promise<any>)[]]): Promise<any> {
-     let d= undefined
-     let x;
-     for (let y of fns){
-       try {
-      x = await y(d)
-      d=x
-       }
-       catch{
-        d= await wait2sec(y(d),1);
-       }
-     }
-
- }
